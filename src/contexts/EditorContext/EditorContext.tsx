efdb93554798e35599945bd603f8a8cd1fab4437
flame-eth <theflamepen@gmail.com>
2024-10-03 23:47:00 +0100

    feat: Add EditorContext for managing editor state

---
+++ src/contexts/EditorContext/EditorContext.tsx
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+import { EditorContextType } from "./types";
+
+// Initial state for the editor
+const defaultEditorState: EditorContextType = {
+  editorState: {
+    title: "",
+    content: "",
+    image: "",
+    videoEmbed: null,
+    socialEmbed: null,
+  },
+  updateTitle: () => {},
+  updateContent: () => {},
+  updateImage: () => {},
+  updateVideoEmbed: () => {},
+  updateSocialEmbed: () => {},
+  resetEditor: () => {},
+};
+
+const EditorContext = createContext<EditorContextType>(defaultEditorState);
+
+export default EditorContext;
