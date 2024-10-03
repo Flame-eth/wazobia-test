1255cb1f899861e1b5806766f5b907b7e81dc766
flame-eth <theflamepen@gmail.com>
2024-10-03 23:14:39 +0100

    feat: Add initial project files and configuration

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+const Home: FC = () => {
+  return (
+    <div className=' w-full h-screen py-5 md:py-10 bg-white '>
+        <div className=" border border-gray-200 rounded-md w-full max-w-[80%] mx-auto md:max-w-md lg:max-w-5xl h-full ">
+            <div className=" mt-10 border border-gray-100"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
