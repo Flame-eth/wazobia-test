4408811aa072e37626380bd377caba0fef9341b5
flame-eth <theflamepen@gmail.com>
2024-10-04 11:23:18 +0100

    refactor: Update Popover component styling for better user experience

--- src/components/embed/EmbedOptions.tsx
+++ src/components/embed/EmbedOptions.tsx
@@ -1,22 +1,25 @@
 import { FC } from "react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Plus } from "lucide-react";
+import ImageEmbed from "./ImageEmbed";
 
 const EmbedOptions: FC = () => {
   return (
     <Popover>
       <PopoverTrigger>
-        <Plus size={35} className=" bg-gray-200 rounded-full p-1" />
+        <Plus size={35} className=" bg-[#E7F1E9] rounded-full p-1" />
       </PopoverTrigger>
       <PopoverContent
         side="bottom"
         align="start"
-        className=" flex flex-col gap-10 justify-start bg-white rounded-md  "
+        className=" flex flex-col justify-start bg-white rounded-md p-0 "
       >
-        <h1 className=" font-light uppercase tracking-wider font-Poppins text-base">
+        <h1 className=" p-3 font-light uppercase tracking-wider font-Poppins text-base">
           EMBEDS
         </h1>
-        <div className="flex flex-col gap-5"></div>
+        <div className="flex flex-col gap-5">
+          <ImageEmbed />
+        </div>
       </PopoverContent>
     </Popover>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Title } from "./title/Title";
 export { default as Content } from "./content/Content";
 export { default as EmbedOptions } from "./embed/EmbedOptions";
+export { default as ImageEmbed } from "./embed/ImageEmbed";
