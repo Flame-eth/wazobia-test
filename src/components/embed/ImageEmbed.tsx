764140db9544f1834f322cce6830465e54cf44a2
flame-eth <theflamepen@gmail.com>
2024-10-04 11:22:57 +0100

    refactor: Update ImageEmbed component to use Dialog component for displaying image details

--- src/components/embed/ImageEmbed.tsx
+++ src/components/embed/ImageEmbed.tsx
@@ -1,10 +1,27 @@
 import { FC } from 'react'
+import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog'
+import { Image } from 'lucide-react'
 
 const ImageEmbed: FC = () => {
   return (
-    <div className=''>
-
-    </div>
+   <Dialog>
+    <DialogTrigger asChild>
+        <div className=" flex gap-2 items-start bg-white cursor-pointer hover:bg-[#E7F1E9] p-3">
+            <Image size={20} />
+            <div className="flex flex-col gap-1">
+                <h1 className=" font-medium capitalize tracking-wider font-Poppins text-base">
+                    Picture
+                </h1>
+                <p className=" font-light font-Poppins text-sm text-black/60">
+                    Jpeg, png
+                </p>
+            </div>
+        </div>
+    </DialogTrigger>
+    <DialogContent>
+        
+    </DialogContent>
+    </Dialog>
   )
 }
 
