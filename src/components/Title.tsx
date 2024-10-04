780914eb964d298b183b4e327ea8a68fb3647b33
flame-eth <theflamepen@gmail.com>
2024-10-04 08:02:28 +0100

    refactor: Update Title component to use Input component for better user experience

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,23 @@
-import { FC } from 'react'
+import { useEditorContext } from "@/contexts";
+import { ChangeEvent, FC } from "react";
+import { Input } from "./ui/input";
 
 const Title: FC = () => {
+  const { editorState, updateTitle } = useEditorContext();
   return (
-    <div>Title</div>
-  )
-}
+    <Input
+      placeholder="Add post title"
+      value={editorState.title}
+      className="text-4xl font-bold text-black placeholder:text-black/60 leading-10 outline-none border-none focus-visible:ring-transparent"
+      onChange={(e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        if (e.target.value?.length >= 100) {
+          return;
+        }
+        updateTitle(e.target.value);
+      }}
+    />
+  );
+};
 
-export default Title
+export default Title;
