5188f5d55fb9df520983ba63d2aae94eb1103e2a
flame-eth <theflamepen@gmail.com>
2024-10-04 08:02:35 +0100

    refactor: Update Editor component to include Title component for better organization

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,10 +1,14 @@
+import { Title } from '@/components'
 import { FC } from 'react'
 
 const Editor: FC = () => {
   return (
     <div className=' w-full h-screen py-5 md:py-10 bg-white '>
-        <div className=" border border-gray-200 rounded-md w-full max-w-[80%] mx-auto md:max-w-md lg:max-w-5xl h-full ">
+        <div className=" border border-gray-200 flex flex-col gap-5 md:gap-10 rounded-md w-full max-w-[80%] mx-auto md:max-w-md lg:max-w-5xl h-full ">
             <div className=" mt-10 border border-gray-100"></div>
+            <div className="flex flex-col gap-5 ">
+              <Title />
+            </div>
         </div>
     </div>
   )
