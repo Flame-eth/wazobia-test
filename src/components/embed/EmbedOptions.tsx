4a341b2dc31dd01b7148c16667ebc7be738bfef6
flame-eth <theflamepen@gmail.com>
2024-10-04 10:58:17 +0100

    refactor: Update EmbedOptions component to use Popover component for better user experience

--- src/components/embed/EmbedOptions.tsx
+++ src/components/embed/EmbedOptions.tsx
@@ -1,13 +1,23 @@
 import { FC } from "react";
-import { Popover, PopoverTrigger } from "../ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Plus } from "lucide-react";
 
 const EmbedOptions: FC = () => {
   return (
     <Popover>
       <PopoverTrigger>
-        <Plus size={24} className=" bg-gray-400 rounded-full p-2" />
+        <Plus size={35} className=" bg-gray-200 rounded-full p-1" />
       </PopoverTrigger>
+      <PopoverContent
+        side="bottom"
+        align="start"
+        className=" flex flex-col gap-10 justify-start bg-white rounded-md  "
+      >
+        <h1 className=" font-light uppercase tracking-wider font-Poppins text-base">
+          EMBEDS
+        </h1>
+        <div className="flex flex-col gap-5"></div>
+      </PopoverContent>
     </Popover>
   );
 };
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,21 +1,28 @@
-import { Content, Title } from '@/components'
-import { FC } from 'react'
+import { Content, Title } from "@/components";
+import { useEditorContext } from "@/contexts";
+import { FC, lazy, Suspense } from "react";
+
+const EmbedOptions = lazy(() => import("@/components/embed/EmbedOptions"));
 
 const Editor: FC = () => {
-  
-
+  const { editorState } = useEditorContext();
 
   return (
-    <div className=' w-full h-screen py-5 md:py-10 bg-[#fafafa] '>
-        <div className=" border border-gray-200 flex flex-col gap-5 md:gap-10 rounded-md w-full max-w-[80%] mx-auto md:max-w-xl lg:max-w-5xl h-full ">
-            <div className=" mt-10 border border-gray-100"></div>
-            <div className="flex flex-col gap-5 px-4  h-full ">
-              <Title />
-              <Content />
-            </div>
+    <div className=" w-full h-screen py-5 md:py-10 bg-[#fafafa] ">
+      <div className=" border border-gray-200 flex flex-col gap-5 md:gap-10 rounded-md w-full max-w-[80%] mx-auto md:max-w-xl lg:max-w-5xl h-full ">
+        <div className=" mt-10 border border-gray-100"></div>
+        <div className="flex flex-col gap-5 px-4  h-full ">
+          <Title />
+          <Content />
+          {editorState.content.length > 0 && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <EmbedOptions />
+            </Suspense>
+          )}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
